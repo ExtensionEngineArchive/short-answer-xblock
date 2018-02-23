7d536b7ac17e2ca05fe1ecbb1bf697c68abaf5db
Ivan Ivic <ivivic1992@gmail.com>
2018-02-23 09:32:22 +0100

    Set manually graded items to visible by default
    NDPD-989

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def package_data(pkg, roots):
 
 setup(
     name='short_answer-xblock',
-    version='0.2.4',
+    version='0.2.5',
     description='Short Answer XBlock',
     license='MIT',
     packages=[
