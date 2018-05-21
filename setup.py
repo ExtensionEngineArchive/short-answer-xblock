da524315fee743aff0a76a6b2acf35681bbf1031
Vedran Karacic <karacicvedran@gmail.com>
2018-05-21 15:40:43 +0200

    Set same message for when due date has passed or grades have been published.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def package_data(pkg, roots):
 
 setup(
     name='short_answer-xblock',
-    version='0.2.20',
+    version='0.2.21',
     description='Short Answer XBlock',
     license='MIT',
     packages=[
